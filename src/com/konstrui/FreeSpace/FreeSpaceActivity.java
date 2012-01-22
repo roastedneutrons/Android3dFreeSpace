cf2482406dbd07948f7e2c65a69f438cc5b2326c
Sudhir Palliyil <sudhirpalliyil@gmail.com>
2012-01-22 22:35:55 +0530

    Removed the TextView for the orientation debug dispaly. Now everything happens on a canvas

--- src/com/konstrui/FreeSpace/FreeSpaceActivity.java
+++ src/com/konstrui/FreeSpace/FreeSpaceActivity.java
@@ -1,7 +1,11 @@
 package com.konstrui.FreeSpace;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,7 +14,8 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.media.SoundPool.OnLoadCompleteListener;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -23,6 +28,9 @@ public class FreeSpaceActivity extends Activity implements SensorEventListener {
 	private int soundID;
 	boolean loaded = false;
 
+	private static int displayWidth = 0;
+	private static int displayHeight = 0;
+
 	private float[] mAccelerometerReading;
 	private float[] mMagneticFieldReading;
 	private float[] mRotationMatrix = new float[16];
@@ -32,26 +40,30 @@ public class FreeSpaceActivity extends Activity implements SensorEventListener {
 	private float[] gVec={0,0,0};
 	private float[] curAccel={0,0,0};
 	
+	private GameView gView;
+	
 /** Called when the activity is first created. */
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	  
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		view = (TextView)findViewById(R.id.textView);
-		view.setBackgroundColor(Color.BLACK);
+		Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+		displayWidth = display.getWidth(); 		
+		displayHeight = display.getHeight(); 	
+
+		//setContentView(R.layout.main);
+		gView=new GameView(this);
+		setContentView(gView);
+		//view = (TextView)findViewById(R.id.textView);
+		//view.setBackgroundColor(Color.BLACK);
 
 		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		mSensorManager.registerListener(this,
-				mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-				SensorManager.SENSOR_DELAY_GAME);
+			mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+			SensorManager.SENSOR_DELAY_GAME);
 		mSensorManager.registerListener(this,
-				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
-				SensorManager.SENSOR_DELAY_GAME);
+			mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+			SensorManager.SENSOR_DELAY_GAME);
 		lastUpdate = System.currentTimeMillis();
 
 		this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
@@ -90,7 +102,8 @@ public class FreeSpaceActivity extends Activity implements SensorEventListener {
 			float yaw = mOrientation[0] * 57.2957795f;
 	        float pitch = mOrientation[1] * 57.2957795f;
 	        float roll = mOrientation[2] * 57.2957795f;
-	        view.setText("yaw: "+yaw+"\npitch: "+pitch+"\nroll: "+roll);
+	        gView.debugText="yaw: "+yaw+"\npitch: "+pitch+"\nroll: "+roll;
+	        gView.invalidate();
 		}
 		if (isShaken()) {
 			onShake();
@@ -122,10 +135,10 @@ public class FreeSpaceActivity extends Activity implements SensorEventListener {
 
 	public void onShake() {
 		if (color) {
-			view.setBackgroundColor(Color.BLUE);
+			// TODO view.setBackgroundColor(Color.BLUE);
 			
 		} else {
-			view.setBackgroundColor(Color.RED);
+			// TODO view.setBackgroundColor(Color.RED);
 		}
 		color = !color;
 		
@@ -162,5 +175,41 @@ public class FreeSpaceActivity extends Activity implements SensorEventListener {
 		// unregister listener
 		mSensorManager.unregisterListener(this);
 		super.onStop();
+	}
+	
+	private static class GameView extends View {
+		private static Rect displayRect = null; //rect we display to
+		private Rect scrollRect = null; //rect we scroll over our bitmap with
+		public String debugText="";
+
+		
+		public GameView(Context context) {
+			super(context);
+			displayRect = new Rect(0, 0, displayWidth, displayHeight);
+			scrollRect = new Rect(0, 0, displayWidth/4, displayHeight/4);
+		}
+		
+		protected void onDraw(Canvas canvas) {
+			canvas.drawColor(Color.BLACK);
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+			paint.setColor(Color.BLUE);
+			paint.setStyle(Paint.Style.STROKE);
+			canvas.drawRect(scrollRect, paint);
+
+			paint.setAntiAlias(true);
+			paint.setTextSize(30);
+			//canvas.drawText(debugText, 0, 30, paint);
+			drawMultilineText(debugText, 0, 30, paint,canvas);
+		}
+
+		private void drawMultilineText(String text, float x, float y, Paint paint, Canvas canvas){
+			final float textSize = paint.getTextSize();
+			for(String line: text.split("\n")){
+			      canvas.drawText(line, x, y, paint);
+			      y+=textSize; //or whatever the font height is....
+			}
+			
+		}
 	}
 }
